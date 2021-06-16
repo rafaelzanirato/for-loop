05a06f5f76b0d6595ccf31e054f31b8b89a1843c
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-06-16 17:09:56 -0300

    Add files via upload

---
+++ rafael_210615.py
@@ -0,0 +1,45 @@
+# Question 2 e 3 
+red=[]
+for value in range(4,8):
+    red.append(value)
+print(red)
+# Queston 4 e 5
+pig= list(range(1,11))
+print(pig)
+# Question 6
+for value in range(10):
+   print("Rafael")
+# Question 7 e 8
+squares=[]
+for value in range(1,11):
+    squares.append("Rafael")
+print(squares)
+# Questions 9 e 10
+egg=[]
+for value in range(5):
+    egg.append("rafael")
+for value in range(5):
+    egg.append("RAFAEL")
+print (egg)
+# Questions 11, 12 e 13
+lion=[]
+for value in range(10):
+    lion.append("rafael")
+print(lion)
+for value in range(5,10):
+    lion[value]="RAFAEL"
+print(lion)
+# Question 14
+green=[]
+pink=1
+for value in range(7):
+    pink=pink+value
+    green.append(pink)
+print(green)
+
+
+
+
+
+
+
